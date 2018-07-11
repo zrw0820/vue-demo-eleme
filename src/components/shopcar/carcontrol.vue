80caf3860cb670f2bfe1b0512459f76d59f865e5
zouruwang <435068084@qq.com>
2018-07-11 18:50:39 +0800

    增加动画效果

--- src/components/shopcar/carcontrol.vue
+++ src/components/shopcar/carcontrol.vue
@@ -1,12 +1,16 @@
 <!-- shop car add and sub control -->
 <template>
   <div class="control">
-    <div class="sub" @click="sub()" v-show="food.count > 0">
-      <i class="icon-remove_circle_outline"></i>
-    </div>
-    <div class="count" v-show="food.count > 0">
-      {{food.count}}
-    </div>
+    <transition name="move">
+      <div class="sub" @click="sub()" v-show="food.count > 0">
+        <i class="icon-remove_circle_outline"></i>
+      </div>
+    </transition>
+    <transition name="move">
+      <div class="count" v-show="food.count > 0">
+        {{food.count}}
+      </div>
+    </transition>
     <div class="add" @click="add()">
       <i class="icon-add_circle"></i>
     </div>
@@ -50,4 +54,9 @@ export default {
       margin-bottom 3px
       font-size 10px
       color rgb(147, 153, 159)
+    .move-enter-active, .move-leave-active
+      transition: all 0.5s
+    .move-enter, .move-leave-to
+      opacity: 0
+      transform translate3d(24px, 0, 0)
 </style>
